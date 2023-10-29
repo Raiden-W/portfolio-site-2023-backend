c699b163e794a5ef68667aa90dccbb9d7c1a33ac
Zijun Wang <zijunwang2020@gmail.com>
2023-10-29 16:30:47 +0000

    fixed package.json

--- config/middlewares.js
+++ config/middlewares.js
@@ -26,19 +26,18 @@ module.exports = ({ env }) => [
       },
     },
   },
-  // {
-  //   name: "strapi::cors",
-  //   config: {
-  //     origin: [
-  //       env("EXTERNAL_URL", `http://localhost:${env("PORT", "1337")}`),
-  //       env("FRONT_END_ORIGIN", "http://localhost:5173"),
-  //     ],
-  //     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-  //     headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-  //     keepHeaderOnError: true,
-  //   },
-  // },
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [
+        env("EXTERNAL_URL", `http://localhost:${env("PORT", "1337")}`),
+        env("FRONT_END_ORIGIN", "http://localhost:5173"),
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
