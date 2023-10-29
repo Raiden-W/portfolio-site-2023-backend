c3426a4592519014fc4f6b359ed6f054e125af27
Zijun Wang <zijunwang2020@gmail.com>
2023-10-29 01:11:29 +0100

    untrack lock

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,8 +1,6 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
-console.log("in production!");
-
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
